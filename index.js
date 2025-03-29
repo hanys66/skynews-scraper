0aaf5b7e5b30250e314b150f8f9c8c34b3a1ca59
hany shaker <hanyshaker@hotmail.com>
2025-03-29 22:27:34 +0200

    Update index1

--- index.js
+++ index.js
@@ -58,7 +58,8 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+//app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
 
