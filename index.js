f3c452399a1685a22d35d46bdf779568c6072faa
hany shaker <hanyshaker@hotmail.com>
2025-03-29 22:54:56 +0200

    Update index2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const { chromium } = require('playwright');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
